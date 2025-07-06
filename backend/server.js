7b385b64df9c73e0c43842a10cd23ce9134c88a3
sunny55g <140750855+sunny55g@users.noreply.github.com>
2025-07-06 20:30:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['https://educonsultant.netlify.app/', 'http://localhost:3000', 'http://127.0.0.1:5500'],
+    origin: ['https://educonsultant.netlify.app', 'http://localhost:3000', 'http://127.0.0.1:5500'],
     credentials: true
 }));
 app.use(express.json());
